e399a91a6b5ccb03b6419f08e4c94eaaad04e127
Selitskii <Ilikedeadline@gmail.com>
2021-07-05 11:38:16 +0300

    Notification Rate 1.0

---
+++ app/src/main/java/thesis_project/data/worker/WorkerNotificationRate.kt
@@ -0,0 +1,148 @@
+package thesis_project.data.worker
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.telephony.AvailableNetworkInfo
+import androidx.core.app.NotificationCompat
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.thesis_project.R
+import thesis_project.Dependencies
+import thesis_project.domain.use_case.SharedPreferencesRateDoubleUseCase
+import thesis_project.domain.use_case.SharedPreferencesSwitchUseCase
+import thesis_project.presentation.ui.start.StartActivity
+
+class WorkerNotificationRate(
+    appContext: Context,
+    params: WorkerParameters
+) : CoroutineWorker(appContext, params) {
+
+    private var notificationManager: NotificationManager? = null
+    private val NOTIFY_ID = 2
+    private val CHANNEL_ID = "CHANNEL_ID"
+    private var eur_in: Double = 0.0
+    private var usd_in: Double = 0.0
+    private var rub_in: Double = 0.0
+    private var uah_in: Double = 0.0
+    private var rub_out: Double = 0.0
+    private var usd_out: Double = 0.0
+    private var eur_out: Double = 0.0
+    private var uah_out: Double = 0.0
+
+
+    private var notificationRub: Boolean = false
+    private var notificationUsd: Boolean = false
+    private var notificationUah: Boolean = false
+    private var notificationEur: Boolean = false
+
+    val key_switchRub = "RUB_SWITCHNEWS"
+    val key_switchUsd = "USD_SWITCHNEWS"
+    val key_switchUah = "UAH_SWITCHNEWS"
+    val key_switchEur = "EUR_SWITCHNEWS"
+
+    val key_BuyRub = "RUB_BUY"
+    val key_BuyUsd = "USD_BUY"
+    val key_BuyUah = "UAH_BUY"
+    val key_BuyEur = "EUR_BUY"
+    val key_SaleRub = "RUB_SALE"
+    val key_SaleUsd = "USD_SALE"
+    val key_SaleUah = "UAH_SALE"
+    val key_SaleEur = "EUR_SALE"
+
+    val sharedPreferencesSwitch: SharedPreferencesSwitchUseCase by lazy { Dependencies.getSharedPreferenceSwitch() }
+    val sharedPreferencesRate: SharedPreferencesRateDoubleUseCase by lazy { Dependencies.getSharedPreferenceRate() }
+
+
+    override suspend fun doWork(): Result {
+
+        val callRate = Dependencies.getRateCloudUseCase().getRateCountry()
+
+        rub_in = takeRateSharedPreferences(key_BuyRub)
+        usd_in = takeRateSharedPreferences(key_BuyUsd)
+        eur_in = takeRateSharedPreferences(key_BuyEur)
+        uah_in = takeRateSharedPreferences(key_BuyUah)
+        rub_out = takeRateSharedPreferences(key_SaleRub)
+        usd_out = takeRateSharedPreferences(key_SaleUsd)
+        uah_out = takeRateSharedPreferences(key_SaleUah)
+        eur_out = takeRateSharedPreferences(key_SaleEur)
+
+        notificationRub = takeStatusSwitch(key_switchRub)
+        notificationUsd = takeStatusSwitch(key_switchUsd)
+        notificationUah = takeStatusSwitch(key_switchUah)
+        notificationEur = takeStatusSwitch(key_switchEur)
+
+        if (callRate.isSuccessful) {
+            callRate.body()?.forEach { rate ->
+                if (notificationRub == true) {
+                    if ((rate.rub_in.toDouble() >= rub_in) || (rate.rub_out.toDouble() <= rub_out)) {
+                        showNotification("Изменился Рубль!!!")
+                    }
+                }
+                if (notificationUsd == true) {
+                    if ((rate.usd_in.toDouble() >= usd_in) || (rate.usd_out.toDouble() <= usd_out)) {
+                        showNotification("Изменился Доллар!!!")
+                    }
+                }
+                if (notificationUah == true) {
+                    if ((rate.uah_in.toDouble() >= uah_in) || (rate.uah_out.toDouble() <= uah_out)) {
+                        showNotification("Изменился Юань!!!")
+                    }
+                }
+                if (notificationEur == true) {
+                    if ((rate.euro_in.toDouble() >= eur_in) || (rate.euro_out.toDouble() <= eur_out)) {
+                        showNotification("Изменился Евро!!!")
+                    }
+                }
+            }
+        }
+        return Result.success()
+    }
+
+    //канал для уведомлений
+    fun createChannelIfNeeded(manager: NotificationManager) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel =
+                NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_DEFAULT)
+            manager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    fun takeStatusSwitch(key: String): Boolean {
+        return sharedPreferencesSwitch.take(key)
+    }
+
+    fun takeRateSharedPreferences(key: String): Double {
+        return sharedPreferencesRate.take(key)
+    }
+
+    fun showNotification(text: String) {
+        notificationManager =
+            (applicationContext.getSystemService(Context.NOTIFICATION_SERVICE)) as NotificationManager
+        val intent = Intent(applicationContext, StartActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        val pendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val notificationBuilder =
+            NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                .setAutoCancel(true)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setWhen(System.currentTimeMillis())
+                .setContentIntent(pendingIntent)
+                .setContentTitle("БеларусьБанк")
+                .setContentText(text)
+                .setPriority(AvailableNetworkInfo.PRIORITY_HIGH);
+        createChannelIfNeeded(notificationManager ?: return)
+        (notificationManager ?: return).notify(
+            NOTIFY_ID,
+            notificationBuilder.build()
+        )
+    }
+}
